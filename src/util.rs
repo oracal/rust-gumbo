f312eaa1975895fc30a990b4762d231f6f9fdeb0
Thomas Whitton <mail@thomaswhitton.com>
2014-07-27 11:55:25 +0100

    transmute directly into a reference rather than going the long way round

--- src/util.rs
+++ src/util.rs
@@ -7,7 +7,7 @@ pub fn gumbo_vector_to_vector<T>(gumbo_vector: &ffi::GumboVector) -> Vec<&T> {
     if gumbo_vector.data.is_not_null() {
         for index in range(0i, gumbo_vector.length as int) {
             unsafe {
-                vec.push(&*mem::transmute::<*mut c_void, *mut T>(*(gumbo_vector.data.offset(index))));
+                vec.push(mem::transmute::<*mut c_void, &T>(*(gumbo_vector.data.offset(index))));
             }
         }
     }
